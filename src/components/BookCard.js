4da452e4cb663d2b9d03f4c0ca37c3be58ca9f7c
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-04 12:45:09 -0600

    Add book card props

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const BookCard = () => (
+const BookCard = ({ bookData }) => (
   <div className="book-card d-flex">
     <div className="left-part flex-column">
-      <p className="category">Action</p>
-      <h2 className="book-title">The Hunger Games</h2>
-      <blockquote className="book-author">Suzanne Collins</blockquote>
+      <p className="category">{bookData.category}</p>
+      <h2 className="book-title">{bookData.title}</h2>
+      <blockquote className="book-author">{bookData.author}</blockquote>
       <div className="options d-flex">
         <p className="option">Comments</p>
         <div className="line" />
@@ -17,18 +18,26 @@ const BookCard = () => (
     <div className="center-part d-flex">
       <div className="circular-progress" />
       <div className="data">
-        <p className="percentage">64%</p>
+        <p className="percentage">
+          {bookData.percentage}
+          {' '}
+          %
+        </p>
         <p className="completed gray">Completed</p>
       </div>
     </div>
     <div className="big-line" />
     <div className="right-part flex-column">
       <p className="gray">Current chapter</p>
-      <p>Chapter 17</p>
+      <p>{bookData.chapter}</p>
       <button type="button" className="update-btn">Update progress</button>
 
     </div>
   </div>
 );
+
+BookCard.propTypes = {
+  bookData: PropTypes.shape.isRequired,
+};
 
 export default BookCard;
--- src/components/Books.js
+++ src/components/Books.js
@@ -2,7 +2,41 @@ import React from 'react';
 import BookCard from './BookCard';
 
 const Books = () => (
-  <BookCard />
+  <>
+    <BookCard
+      bookData={
+        {
+          title: 'The Hunger Games',
+          category: 'Action',
+          author: 'Suzanne Collins',
+          percentage: '64',
+          chapter: 'Chapter 17',
+        }
+      }
+    />
+    <BookCard
+      bookData={
+        {
+          title: 'Dune',
+          category: 'Science fiction',
+          author: 'Frank Herbert',
+          percentage: '8',
+          chapter: 'Chapter3:"A Lesson Learned"',
+        }
+      }
+    />
+    <BookCard
+      bookData={
+        {
+          title: 'Capital in the Twenty-First Century',
+          category: 'Economy',
+          author: 'Thomas Piketty',
+          percentage: '0',
+          chapter: 'Introduction',
+        }
+      }
+    />
+  </>
 );
 
 export default Books;
