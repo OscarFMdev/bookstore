d42f87511998c16630ded71144bf81e73125d541
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-04 11:37:01 -0600

    Add styles to BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookCard = () => (
   <div className="book-card d-flex">
-    <div className="left-part d-flex flex-column">
+    <div className="left-part flex-column">
       <p className="category">Action</p>
       <h2 className="book-title">The Hunger Games</h2>
       <blockquote className="book-author">Suzanne Collins</blockquote>
@@ -13,6 +13,20 @@ const BookCard = () => (
         <div className="line" />
         <p className="option">Edit</p>
       </div>
+    </div>
+    <div className="center-part d-flex">
+      <div className="circular-progress" />
+      <div className="data">
+        <p className="percentage">64%</p>
+        <p className="completed gray">Completed</p>
+      </div>
+    </div>
+    <div className="big-line" />
+    <div className="right-part flex-column">
+      <p className="gray">Current chapter</p>
+      <p>Chapter 17</p>
+      <button type="button" className="update-btn">Update progress</button>
+
     </div>
   </div>
 );
