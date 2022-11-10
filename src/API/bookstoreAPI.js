7e1d6a36165bfa1bfe18a3dc1d5734c84c1bbcf7
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-10 13:37:49 -0600

    Add functions for fetch, delete and add API

--- src/API/bookstoreAPI.js
+++ src/API/bookstoreAPI.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
-const bookstoreAPI = axios.create({
-  baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi',
+const APP_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/tAB8SosTtsDw2NxkYEWP/books';
+
+export const fetchBooksAPI = async () => {
+  const response = await axios.get(APP_URL);
+  return response.data;
+};
+
+export const deleteBookAPI = async (id) => axios({
+  method: 'DELETE',
+  url: `${APP_URL}/${id}`,
 });
 
-export default bookstoreAPI;
+export const addBookAPI = async (book) => axios({
+  method: 'POST',
+  url: APP_URL,
+  data: book,
+});
